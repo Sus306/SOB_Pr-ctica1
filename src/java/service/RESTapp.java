c4022d5b670fd9eff06995391486ffeb6cdbd28e
Susana Triviño Nortes <susana.trivino@estudiants.urv.cat>
2024-10-29 15:33:38 +0100

    Primer Commit

--- src/java/service/RESTapp.java
+++ src/java/service/RESTapp.java
@@ -4,5 +4,5 @@ import jakarta.ws.rs.core.Application;
 
 @jakarta.ws.rs.ApplicationPath("webresources")
 public class RESTapp extends Application {
-    
+            //homework1/rest/api/v1/art....y no webresources
 }
